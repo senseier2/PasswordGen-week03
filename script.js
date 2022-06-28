5772cda145f3847ab1eeb87dc9dce6b3dd14945f
Senfire <eric.mcghee@northwestern.edu>
2022-06-28 15:49:17 -0500

    Adding functions for generate password and write password (not working)

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var passwordLength = 0;
 var choiceArray = [];
 
 //Arrays used to create password characters
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", ".", "/", "?", ";"]
-var numberArrey = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", ".", "/", "?", ";"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lcaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var ucaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -14,17 +14,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var myPrompts = getPrompts(); //returns a boolean
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if(myPrompts) {
+    var yourPassword = generatePassword();
+    passwordText.value = yourPassword;
+  } 
 
+  else {
+    passwordTest.value = "";
+  }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{
 
-const aNumber = Number(window.prompt("How many characters in your password?", ""));
+}
+
+
 /*
 Generate password
   User input a number between 8 and 128. (stored in a varible)
@@ -40,15 +48,39 @@ Generate password
 //Generate a password based on the above function call from writePassword
 function generatePassword() {
 
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+      var randomChar = Math.floor(Math.random() * choiceArray.length);
+      password = password + choiceArray[randomChar];
+  }
+  return password;
 }
 
 
 //Create prompts for character types as in the User request described
 function charPrompts() {
+  choiceArray = [];
   passwordLength = Number(prompt("How many characters will your password have? (Please chose a number between 8 to 128"))
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert ("Character length has to be a number, and between 8 - 128. Try Again!");
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Character length has to be a number, and between 8 - 128. Try Again!");
     return false;
   }
+
+  if (confirm("Would you like to have lowercase letters in your password")) {
+    choiceArray = choiceArray.concat(lcaseArray);
+  }
+
+  if (confirm("Would you like to have uppercase letters in your password")) {
+    choiceArray = choiceArray.concat(ucaseArray);
+  }
+
+  if (confirm("Would you like to have symbols in your password")) {
+    choiceArray = choiceArray.concat(symbols);
+  }
+
+  if (confirm("Would you like to have lowercase letters in your password")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+  return true;
 }
