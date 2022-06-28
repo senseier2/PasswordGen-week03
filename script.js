ace427aa9a340427a500fc4dbb555259801fd050
Senfire <eric.mcghee@northwestern.edu>
2022-06-28 16:23:54 -0500

    Bug fixes for the duplicated text and the broken generate call

--- script.js
+++ script.js
@@ -12,26 +12,23 @@ var ucaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-    var myPrompts = getPrompts(); //returns a boolean
+    var myPrompts = charPrompts(); //returns a boolean
     var passwordText = document.querySelector("#password");
 
   if(myPrompts) {
     var yourPassword = generatePassword();
     passwordText.value = yourPassword;
   } 
-
   else {
     passwordTest.value = "";
   }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
-
-}
-
 
 /*
 Generate password
@@ -63,7 +60,7 @@ function charPrompts() {
   passwordLength = Number(prompt("How many characters will your password have? (Please chose a number between 8 to 128"))
 
   if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Character length has to be a number, and between 8 - 128. Try Again!");
+    alert("Character length has to be a number, and between 8 - 128.");
     return false;
   }
 
@@ -79,7 +76,7 @@ function charPrompts() {
     choiceArray = choiceArray.concat(symbols);
   }
 
-  if (confirm("Would you like to have lowercase letters in your password")) {
+  if (confirm("Would you like to have numbers in your password")) {
     choiceArray = choiceArray.concat(numberArray);
   }
   return true;
