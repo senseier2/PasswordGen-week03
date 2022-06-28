4984825e3a6eaeb0b71d682dc49ccc841c5eecb3
Senfire <eric.mcghee@northwestern.edu>
2022-06-28 11:49:36 -0500

    Initial update with varible tests and array test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # PasswordGen-week03
 Password Generator Application
+
+Jun 27 2022
+
+To create a password generator that accepts user input to generate a password. When the user accesses the application they will be prompted to input the following:
+ 
+ 1. the number of characters used (between 8 to 128)
+ 2. Choice to use lowercase letters
+ 3. Choice to use uppercase letters
+ 4. Choice to use numbers
+ 5. Choice to use symbols
+
+ Following the verification of these choices a random password will be returned as long as one choice of character type has been confirmed.
+
+ 
---
+++ script.js
@@ -0,0 +1,42 @@
+var passwordLength = 0;
+var choiceArray = [];
+
+//Arrays used to create password characters
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", ".", "/", "?", ";"]
+var numberArrey = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lcaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ucaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+const aNumber = Number(window.prompt("How many characters in your password?", ""));
+/*
+Generate password
+  User input a number between 8 and 128. (stored in a varible)
+  list all of the characters possible in an array
+    array lowercase letters
+    array of uppercase letters
+    array of numbers
+    array of symbols
+
+  I will need varibles to hold the user inputs
+*/
+
+
+
+
